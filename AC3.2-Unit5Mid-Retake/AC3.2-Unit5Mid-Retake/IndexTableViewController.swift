bbbe1499295f51a9c06b394e46f8a81716fa9069
Louis Tur <louis.tur@gmail.com>
2017-01-05 15:07:33 -0500

    base commit; tableVC added + utilities

---
+++ AC3.2-Unit5Mid-Retake/AC3.2-Unit5Mid-Retake/IndexTableViewController.swift
@@ -0,0 +1,95 @@
+//
+//  IndexTableViewController.swift
+//  AC3.2-Unit5-MidUnitAssesment
+//
+//  Created by Louis Tur on 12/22/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+/*
+ ******************************************************************
+  
+    ATTENTION AC3.2 STUDENTS
+ 
+      DO NOT MODIFY THIS FILE! 😤
+ 
+ ******************************************************************
+ */
+
+enum TestSections: String {
+  case autoLayout = "AutoLayout"
+  case coreData = "Core Data"
+  
+  static let sections: [String] = [TestSections.autoLayout,
+                                   TestSections.coreData].map { $0.rawValue }
+  
+  static func numberOfTestSections() -> Int {
+    return TestSections.sections.count
+  }
+}
+
+class IndexTableViewController: UITableViewController {
+  
+  // Assesment View Controllers
+  private let autoLayoutViewControllers: [CellTitled] = []
+  private let coreDataViewControllers: [CellTitled] = []
+  
+  let cellIdentifier: String = "IndexCellIdentifier"
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    self.title = "AC3.2 Mid Unit 5"
+  }
+  
+  
+  // MARK: - Table view data source
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return TestSections.numberOfTestSections()
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    switch section {
+    case 0:
+      return autoLayoutViewControllers.count
+    default:
+      return coreDataViewControllers.count
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+    switch (indexPath.section, indexPath.row) {
+    case (0, let row):
+      cell.textLabel?.text = autoLayoutViewControllers[row].titleForCell
+    case (1, 0):
+      cell.textLabel?.text = coreDataViewControllers[0].titleForCell
+    default:
+      print("Add row")
+    }
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return TestSections.sections[section]
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    switch (indexPath.section, indexPath.row) {
+    case (0, let row):
+      let dtvc = autoLayoutViewControllers[row] as! UIViewController
+      navigationController?.pushViewController(dtvc, animated: true)
+    case (1, 0):
+      // TODO: Add Core Data VC
+      print("TODO: Add Core Data VC")
+      
+    default:
+      print("Add row")
+    }
+  }
+}
---
+++ AC3.2-Unit5Mid-Retake/AC3.2-Unit5Mid-Retake/Utilities.swift
@@ -0,0 +1,14 @@
+//
+//  Utilities.swift
+//  AC3.2-Unit5-MidUnitAssesment
+//
+//  Created by Louis Tur on 12/22/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Protocols
+protocol CellTitled {
+  var titleForCell: String { get }
+}
